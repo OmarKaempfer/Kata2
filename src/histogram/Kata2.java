776910f1d587fd9c439f22f282536cb97b3920ad
Omar Kämpfer <omarkaempfer@gmail.com>
2018-09-27 09:55:19 +0100

    V5 - Further testing of the Histogram generic type functionality
    Changes on the main method to test a String[] data instead of Integer[] data.

--- src/histogram/Kata2.java
+++ src/histogram/Kata2.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
-        Integer[] data = new Integer[] {1, 2, 5, -10, 20, 74, -2, 2, 0, 5, -4, 5, -7, 1};
+        String[] data = new String[] {"Rosa", "Pepe","María","Pepe", "Pepe", "Rosa"};
         Histogram histogram = new Histogram(data);
-        Map<Integer, Integer> histogramMap = histogram.getHistogram();
-        for(int key : histogramMap.keySet()) {
+        Map<String, Integer> histogramMap = histogram.getHistogram();
+        for(String key : histogramMap.keySet()) {
             System.out.println(key + " : " + histogramMap.get(key));
         }
     }
