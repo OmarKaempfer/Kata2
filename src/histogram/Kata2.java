f4e7b26d8bfcd5be735884101f7471bfab7fbe12
Omar Kämpfer <omarkaempfer@gmail.com>
2018-09-27 09:44:33 +0100

    V3 - Refactoring the histogram logic
    The histogram is now a class by itself with all the logic regarding the data parsing and its own attribute to store the parsed data.

--- src/histogram/Kata2.java
+++ src/histogram/Kata2.java
@@ -6,17 +6,10 @@ import java.util.Map;
 public class Kata2 {
     public static void main(String[] args) {
         int[] data = new int[] {1, 2, 5, -10, 20, 74, -2, 2, 0, 5, -4, 5, -7, 1};
-        Map<Integer, Integer> histogram = getHistogramMap(data);
-        for(int key : histogram.keySet()) {
-            System.out.println(key + " : " + histogram.get(key));
+        Histogram histogram = new Histogram(data);
+        Map<Integer, Integer> histogramMap = histogram.getHistogram();
+        for(int key : histogramMap.keySet()) {
+            System.out.println(key + " : " + histogramMap.get(key));
         }
-    }
-
-    public static Map<Integer, Integer> getHistogramMap(int[] data) {
-        Map<Integer, Integer> histogram = new HashMap<>();
-        for(int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-        }
-        return histogram;
     }
 }
