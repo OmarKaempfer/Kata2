496ce9043bd038758529dafaedc2610ba1b27242
Omar Kämpfer <omarkaempfer@gmail.com>
2018-09-27 09:51:45 +0100

    V4 - Histogram now supports generic types
    Generic types support was implemented in the histogram class in order to allow the making of histograms of any kind of keys, despite its class.

--- src/histogram/Kata2.java
+++ src/histogram/Kata2.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
-        int[] data = new int[] {1, 2, 5, -10, 20, 74, -2, 2, 0, 5, -4, 5, -7, 1};
+        Integer[] data = new Integer[] {1, 2, 5, -10, 20, 74, -2, 2, 0, 5, -4, 5, -7, 1};
         Histogram histogram = new Histogram(data);
         Map<Integer, Integer> histogramMap = histogram.getHistogram();
         for(int key : histogramMap.keySet()) {
